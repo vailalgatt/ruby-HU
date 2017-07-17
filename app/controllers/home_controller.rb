10166ed2a86cc5b9c49ecff6aec1f4d1d7fbbcd3
Vail Algatt <vail.algatt@gmail.com>
2017-07-16 21:54:42 -0600

    questions are ordered by time posted, and questions are being submitted as an array of objects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @questions = Question.order(created_at :desc).all
   end
 
   def about 
-  end
-
-  def temp
-    redirect_to root_path 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :answers
+
+  resources :questions
+
   root 'home#index'
 
   get '/about' => 'home#about'
-
-  post '/questions' => 'home#temp'
-  post '/answers' => 'home#temp'
-
-  get '/questions/:id' => 'home#question'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
