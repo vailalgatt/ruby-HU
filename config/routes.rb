95b635805237249f4aede714730162d91d459e08
Vail Algatt <vail.algatt@gmail.com>
2017-07-16 21:21:10 -0600

    added new answer partial. users can ask and answer questions, and questions route to their own page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  # get 'home/index'
 
   get '/about' => 'home#about'
+
+  post '/questions' => 'home#temp'
+  post '/answers' => 'home#temp'
+
+  get '/questions/:id' => 'home#question'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
