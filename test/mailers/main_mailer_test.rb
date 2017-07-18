677fb36120f26928bf3a1f90e3a613d544e6bf6e
Vail Algatt <vail.algatt@gmail.com>
2017-07-18 16:56:30 -0600

    appending correlating answers to questions for email notifications

--- test/mailers/main_mailer_test.rb
+++ test/mailers/main_mailer_test.rb
@@ -2,10 +2,18 @@ require 'test_helper'
 
 class MainMailerTest < ActionMailer::TestCase
   test "notify_question_author" do
-    mail = MainMailer.notify_question_author
+
+    question = Question.create email: 'author@question.com', body: 'a test question'
+    answer = Answer.create email: 'author@answer.com', body: 'a test answer'
+
+    question.answers << answer
+
+    mail = MainMailer.notify_question_author(answer)
     assert_equal "Notify question author", mail.subject
+
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
+    
     assert_match "Hi", mail.body.encoded
   end
 
