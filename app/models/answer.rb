4c73c775013cc5b9767dc12edcafcefc149956b0
Vail Algatt <vail.algatt@gmail.com>
2017-07-17 20:15:40 -0600

    added *has gravatar* module

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,7 @@
 class Answer < ActiveRecord::Base
 
+include HasGravatar
 
   belongs_to :question
-def gravatar
-  "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
-end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,7 @@
 class Question < ActiveRecord::Base
 
-  # include HasGravatar
+  include HasGravatar
 
   has_many :answers
 
-def gravatar
-  "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
-end
 end
